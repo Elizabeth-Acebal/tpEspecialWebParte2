dd5ab39fea648d754948cc210ce33dd5be59bdd3
Elizabeth-Acebal <elyacebal26@gmail.com>
2023-11-08 20:40:03 -0300

    se agregan funciones

--- router-api.php
+++ router-api.php
@@ -1,14 +1,28 @@
 <?php
 require_once 'libs/Router.php';
+require_once 'app/controllers/peliculaApiController.php';
 
 // crea el router
 $router = new Router();
-
+echo "nuevo";
 // define la tabla de ruteo
-$router->addRoute('peliculas', 'GET', 'peliculaApiController', 'getPeliculaConGenero');
-$router->addRoute('peliculas/:ID', 'GET', 'peliculaApiController', 'obtenerPelicula');
+$router->addRoute('peliculas', 'GET', 'peliculaApiController', 'getAll');
+$router->addRoute('peliculasConGenero', 'GET', 'peliculaApiController', 'getPeliculasConGenero');
 
-$router->addRoute('tareas', 'POST', 'TaskApiController', 'crearTarea');
+$router->addRoute('peliculasConGenero/:ID', 'DELETE', 'peliculaApiController', 'delete');
+
+$router->addRoute('peliculasConGenero/:ID', 'PUT', 'peliculaApiController', 'modificar');
+//http://localhost/tpEspecialWeb2Parte2/api/peliculasConGenero/57
+/*{
+
+    "titulo": "Pelicula Modificada",
+    "descripcion":"Pelicula modificada en postman",
+    "director": "juan perez",
+    "calificacion": "apta ",
+    "id_genero": 1
+}*/
+
+
 
 
 // rutea le paso el recurso, y el metodo
