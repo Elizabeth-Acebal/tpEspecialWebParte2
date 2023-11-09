beb5be6332652bc5323ff8db62fcb45fe154801b
Joel Pugliese <joelestebanpugliese@gmail.com>
2023-11-09 16:20:26 -0300

    agrego POST

--- app/controllers/peliculaApiController.php
+++ app/controllers/peliculaApiController.php
@@ -1,74 +1,84 @@
 <?php
-    require_once 'app/models/pelicula.model.php';
-    require_once 'app/models/genero.model.php';
-    require_once 'app/views/api.view.php';
-    require_once 'app/helpers/auth.helper.php';
 
-    class peliculaApiController{
+require_once 'app/models/pelicula.model.php';
+require_once 'app/models/genero.model.php';
+require_once 'app/models/auth.model.php';
+require_once 'app/views/api.view.php';
+require_once 'app/controllers/api.controller.php';
 
-        private $peliculaModel;
-        private $generoModel;
-        private $view;
-        private $authHelper;
-        private $data;
+     class PeliculaApiController extends ApiController {
+       private $model;
 
-        function __construct(){
-            $this->peliculaModel = new PeliculaModel;
-            $this->generoModel= new GeneroModel;
-            $this->view= new APIview;
+       function __construct() {
+         parent::__construct();
+         $this -> model = new PeliculaModel();
+         
+       }
 
-            $this->data = file_get_contents("php://input");
 
-        }
-
-        public function getData(){
-            return json_decode($this->data);
-        }
-
-        public function getAll($params=null){
-            $peliculas=$this->peliculaModel->getPeliculas();
-            var_dump($peliculas);
-        }
-
-        public function getPeliculasConGenero($params=null){
-            $peliculasGenero=$this->peliculaModel->getPeliculaConGenero();
-            $this->view->response($peliculasGenero, 200);
-           // var_dump($peliculasGenero);
-        }
-
-        public function delete($params=null){
-            $pelicula_id= $params[':ID'];
-            $success= $this->peliculaModel->eliminarPelicula($pelicula_id);
-            if($success){
-            $this->view->response("La pelicula con el id=$pelicula_id se borró exitosamente",200);
-            }else{
-                $this->view->response("La pelicula con el id=$pelicula_id no existe",404);
+       function get($params =[]) {
+         if (empty($params)){
+           $peliculas = $this->model->getPeliculas();
+           $this ->view-> response($peliculas, 200);
+          }
+          else{
+            $pelicula =  $this->model->getPelicula($params[":ID"]);
+            if(!empty($pelicula)){
+              return $this->view->response($pelicula, 200);
             }
+            else{
+              $this->view->response(['msg' => 'La tarea con el id='.$params[':ID'].' no existe'], 404);
+            }
+          }
+       }
+
+
+       public function delete($params=null){
+        $pelicula_id= $params[':ID'];
+        $success= $this->model->eliminarPelicula($pelicula_id);
+        if($success){
+        $this->view->response("La pelicula con el id=$pelicula_id se borró exitosamente",200);
+        }else{
+            $this->view->response("La pelicula con el id=$pelicula_id no existe",404);
         }
-        
-        public function modificar($params=null){
-            $pelicula_id= $params[':ID'];
-            $body=$this->getData();
-        
-            $titulo= $body->titulo;
-            $descripcion= $body->descripcion;
-            $director= $body-> director;
-            $calificacion= $body-> calificacion;
-            $id_genero= $body->id_genero;
+      }
+
+       public function modificar($params=null){
+        $pelicula_id= $params[':ID'];
+        $body=$this->getData();
     
-
-            $success= $this->peliculaModel->editarPelicula($titulo, $descripcion, $director, $calificacion,$id_genero,$pelicula_id);
-
-            if($success){
-                $this->view->response("Se actualizo la pelicula con el id=$pelicula_id exitosamente",200);
-
-            }else{
-                $this->view->response("No se pudo actualizar la pelicula",500);
-            }
+        $titulo= $body->titulo;
+        $descripcion= $body->descripcion;
+        $director= $body-> director;
+        $calificacion= $body-> calificacion;
+        $id_genero= $body->id_genero;
 
 
+        $success= $this->model->editarPelicula($titulo, $descripcion, $director, $calificacion,$id_genero,$pelicula_id);
+
+        if($success){
+            $this->view->response("Se actualizo la pelicula con el id=$pelicula_id exitosamente",200);
+
+        }else{
+            $this->view->response("No se pudo actualizar la pelicula",500);
         }
-
 
 
     }
+
+       function create($params = []) {
+         $body = $this->getData();
+
+         
+         $titulo = $body->titulo;
+         $descripcion = $body->descripcion;
+         $director = $body->director;
+         $calificacion = $body->calificacion;
+         $id_genero = $body->id_genero;
+
+         $id = $this -> model -> agregarPelicula($titulo, $descripcion, $director, $calificacion, $id_genero);
+         $this->view->response('la pelicula fue insertada con el id='.$id,201);
+       }
+
+     }
+
--- router-api.php
+++ router-api.php
@@ -1,18 +1,17 @@
 <?php
-require_once 'libs/Router.php';
-require_once 'app/controllers/peliculaApiController.php';
+  require_once 'libs/router.php';
+  require_once 'app/controllers/peliculaApiController.php';
 
-// crea el router
-$router = new Router();
-echo "nuevo";
-// define la tabla de ruteo
-$router->addRoute('peliculas', 'GET', 'peliculaApiController', 'getAll');
-$router->addRoute('peliculasConGenero', 'GET', 'peliculaApiController', 'getPeliculasConGenero');
 
-$router->addRoute('peliculasConGenero/:ID', 'DELETE', 'peliculaApiController', 'delete');
+  $router = new Router();
 
-$router->addRoute('peliculasConGenero/:ID', 'PUT', 'peliculaApiController', 'modificar');
-//http://localhost/tpEspecialWeb2Parte2/api/peliculasConGenero/57
+
+  $router -> addRoute('peliculas', 'GET', 'PeliculaApiController', 'get');
+  $router -> addRoute('peliculas/:ID', 'GET', 'PeliculaApiController', 'get');
+  $router -> addRoute('peliculas', 'POST', 'PeliculaApiController', 'create');
+  $router -> addRoute('peliculas/:ID', 'DELETE', 'PeliculaApiController', 'delete');
+  $router->addRoute('peliculas/:ID', 'PUT', 'peliculaApiController', 'modificar');
+  //http://localhost/tpEspecialWeb2Parte2/api/peliculasConGenero/57
 /*{
 
     "titulo": "Pelicula Modificada",
@@ -26,4 +25,5 @@ $router->addRoute('peliculasConGenero/:ID', 'PUT', 'peliculaApiController', 'mod
 
 
 // rutea le paso el recurso, y el metodo
-$router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
+
+  $router -> route($_GET['resource'], $_SERVER['REQUEST_METHOD']);
